7b0171f059f08a1cb93bebcae5ea601a2c0fef56
Matthew Lewis <matthew.knight.lewis@gmail.com>
2020-10-13 19:20:03 -0400

    First Commit! This program produces random sentences based on a markov chain algorithm and a corprus of late 19th century western genre literature

---
+++ markov.py
@@ -0,0 +1,47 @@
+import random
+import re as regex
+from collections import defaultdict
+
+# Import the text from the path listed.
+path = 'westerns.txt'
+
+# Opening the file and assigning it 'textFile', pull the text into a string.
+with open(path) as textFile:
+    westernText = textFile.read()
+
+# Split the text into individual words, formed as a list. 
+# '\W+' is a regular expression targeting words with one or more characters
+textAsList = [
+    word
+    for word in regex.split('\W+', westernText)
+    if word != ''
+]
+
+# Create graph.
+westernMarkovDictionary = defaultdict(lambda: defaultdict(int))
+
+#Start the last word as the first word in the list, make it lowercase.
+last_word = textAsList[0].lower()
+
+#For each word in the list, make it lowercase, and set the preceding word as a key to find that word
+for word in textAsList[1:]:
+  word = word.lower()
+  westernMarkovDictionary[last_word][word] += 1
+  last_word = word
+
+# The sample string starts with a random word from the textList
+sampleString =  random.choice(textAsList)
+
+# and the last word in the string is set to that word
+lastWord = sampleString
+
+# from 0 to n, the string is appended with a random choice from potential following words
+i = 0
+while i < 40:
+    tempWord = random.choice(list(westernMarkovDictionary[lastWord].keys()))
+    sampleString += ' ' + tempWord
+    lastWord = tempWord
+    i += 1
+
+# and the string is printed!
+print(sampleString)
